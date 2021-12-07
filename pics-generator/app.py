f467929f08f047221db667dd11057ada03962e42
anastasiavdovina <anastasiavdovina21@gmail.com>
2021-12-07 19:03:13 +0300

    add site

---
+++ pics-generator/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import aiohttp_jinja2
+from aiohttp import web
+import aiohttp
+from aiohttp_jinja2 import template
+import jinja2
+import json
+
+json_file = 'result.json'
+
+with open(json_file, 'r') as file:
+    content = file.read()
+    pictures = json.loads(content)
+
+
+async def create_app():
+    app = web.Application()
+    aiohttp_jinja2.setup(app, loader = jinja2.PackageLoader('templates'))
+    setup_routes(app)
+    return app
+
+@template('app.html')
+async def index(request):
+    return {"pictures": pictures}
+
+def setup_routes(app):
+    app.router.add_route('Get', '/', index)
+
+app = create_app()
+
+if __name__ == '__main__':
+    
+    aiohttp.web.run_app(app)
+
+
+'''def parse_json(json_file):
+    with open(json_file, 'r') as file:
+        content = file.read()
+        picture_info = json.loads(content)
+        for picture in picture_info:
+            temp = picture["name"] 
+            source = picture["images"]'''
